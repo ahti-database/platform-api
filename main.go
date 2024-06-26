7ab8ea8830ee4596ee784ff72e89b9ed3465f0d1
Jason Cheng <jasoncky96@gmail.com>
2024-06-26 13:48:12 +0800

    added basic kubernetes connectivity using KUBECONFIG

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/Jason-CKY/ahti/pkg/handlers"
 	"github.com/Jason-CKY/ahti/pkg/utils"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
+
+func getKubernetesConfig() *rest.Config {
+	var config *rest.Config
+	home := homedir.HomeDir()
+	kubeconfig := filepath.Join(home, ".kube", "config")
+	if _, err := os.Stat(kubeconfig); err == nil {
+		// if $HOME/.kube/config file exists
+		log.Infof("Using kubeconfig file at %v", kubeconfig)
+		// use the current context in kubeconfig
+		_config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+		config = _config
+	} else {
+		log.Infof("Using incluster config...")
+		_config, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		config = _config
+	}
+	return config
+}
 
 func main() {
 	// Load environment variables from .env file
@@ -30,6 +62,21 @@ func main() {
 		FullTimestamp:          true,
 		DisableLevelTruncation: true,
 	})
+
+	// create the clientset
+	config := getKubernetesConfig()
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	allConfigmaps, err := clientSet.CoreV1().ConfigMaps("default").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, cm := range allConfigmaps.Items {
+		log.Info(cm.Name)
+	}
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
